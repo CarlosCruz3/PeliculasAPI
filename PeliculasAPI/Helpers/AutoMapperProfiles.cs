1748d8fefdd8621bae1c8fd2091a7d4000842350
Usuario <Usuario@DESKTOP-U9BUVKN>
2023-06-26 10:30:18 -0600

    Agregar archivos de proyecto.

---
+++ PeliculasAPI/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using PeliculasAPI.DTOs;
+using PeliculasAPI.Entidades;
+
+
+namespace PeliculasAPI.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        protected AutoMapperProfiles()
+        {
+            CreateMap<Genero, GeneroDTO>().ReverseMap();
+            CreateMap<GeneroCreacionDTO, Genero>();
+        }
+    }
+}
---
+++ PeliculasAPI/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace PeliculasAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            ////Configuración de los servicios que esta utilizando la api
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
